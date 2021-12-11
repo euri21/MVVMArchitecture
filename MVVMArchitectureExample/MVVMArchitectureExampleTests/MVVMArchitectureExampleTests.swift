45ff18fc313a7093a3d53b1efea9c55e209cfbf7
solution888 <suko.m@outlook.com>
2021-12-11 20:40:24 +0900

    Add unit test

--- MVVMArchitectureExample/MVVMArchitectureExampleTests/MVVMArchitectureExampleTests.swift
+++ MVVMArchitectureExample/MVVMArchitectureExampleTests/MVVMArchitectureExampleTests.swift
@@ -21,6 +21,10 @@ class MVVMArchitectureExampleTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "NewsTableViewController") as! NewsTableViewController
+        vc.loadView()
     }
 
     func testPerformanceExample() throws {
